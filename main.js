f7c4aedba3de40147602688a1d3758f9194e1a31
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2019-12-10 17:24:01 -0600

    Updated File

---
+++ main.js
@@ -0,0 +1,32 @@
+// main.js
+
+var addButton = document.getElementById("add-button");
+addButton.addEventListener("click",addToDoItem);
+
+function addToDoItem(){
+    alert("Add button clicked!");
+}
+
+
+var clearButton = document.getElementById("clear-completed-button");
+addButton.addEventListener("click",ClearCompletedToDoItems);
+
+function ClearCompletedToDoItems(){
+    alert("Cleared Completed Items!");
+}
+
+
+var emptyButton = document.getElementById("empty-button");
+addButton.addEventListener("click",emptyList);
+
+function emptyList(){
+    alert("Emptied Items");
+}
+
+
+var saveButton = document.getElementById("save-button");
+addButton.addEventListener("click",saveList);
+
+function addToDoItem(){
+    alert("Saved items!");
+}
